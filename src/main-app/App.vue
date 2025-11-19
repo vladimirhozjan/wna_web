ce991d178050b8f56f2f95ccf139254924d03cdd
Vladimir Hozjan <vladimir.hozjan@gmail.com>
2025-11-19 12:20:20 +0100

    Prepared to initialize the project

--- src/main-app/App.vue
+++ src/main-app/App.vue
@@ -2,16 +2,19 @@
 
 <template>
 
-  <div id="app">
-    <nav>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/register">Register</router-link>
-    </nav>
-    <router-view />  </div>
+<!--  <div id="app">-->
+<!--    <nav>-->
+<!--      <router-link to="/login">Login</router-link>-->
+<!--      <router-link to="/register">Register</router-link>-->
+<!--    </nav>-->
+<!--    <router-view />  </div>-->
+
 </template>
 
-<style >
+<script>
+</script>
 
+<style >
 /*
   nav { display: flex; gap: 10px; justify-content: center; margin-top: 20px; }
   a { text-decoration: none; color: #42b983; }
