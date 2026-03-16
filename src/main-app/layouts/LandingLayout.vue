564c38bb19e1f985c1a570e69b938219c2f41296
Vladimir Hozjan <vladimir.hozjan@gmail.com>
2026-03-16 19:38:11 +0100

    ID: BUG-040 Title: Public pages light theme only

--- src/main-app/layouts/LandingLayout.vue
+++ src/main-app/layouts/LandingLayout.vue
@@ -18,11 +18,26 @@
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from 'vue'
 import TopNav from '../components/TopNav.vue'
 import PublicFooter from '../components/public/PublicFooter.vue'
-import {authModel} from "../scripts/core/authModel.js";
+import { authModel } from '../scripts/core/authModel.js'
+import { themeModel } from '../scripts/models/themeModel.js'
+
 defineEmits(['open-login', 'open-register'])
 const auth = authModel()
+const theme = themeModel()
+
+let savedMode = null
+
+onMounted(() => {
+  savedMode = theme.mode.value
+  document.documentElement.setAttribute('data-theme', 'light')
+})
+
+onUnmounted(() => {
+  if (savedMode) theme.setMode(savedMode)
+})
 </script>
 
 <style scoped>
